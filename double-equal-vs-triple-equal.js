26c5115661401b30aba36cca1f742bc8ac7610e3
FarhanaRifa <67515279+FarhanaRifa@users.noreply.github.com>
2020-11-04 19:28:42 +0600

    double equal and triple equal meaning

---
+++ double-equal-vs-triple-equal.js
@@ -0,0 +1,17 @@
+// const first = 2;
+// const second = "2";
+//double equal is simply method. that try to convert similar thing with together
+// double equal match there (1 == true)  and (false == 0) and (2=="2");
+
+
+//triple equal will check type and value
+
+const first = 1;
+const second = true;
+
+if( first == second){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
