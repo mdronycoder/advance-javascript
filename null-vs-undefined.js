066c8f8f966254fdfc51131528e01ea1eed8f06a
FarhanaRifa <67515279+FarhanaRifa@users.noreply.github.com>
2020-11-04 16:15:05 +0600

    null vs undefined done

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+// let pakhi; // variable not declare properly then result undefined  
+
+// function add(num1,num2){
+//     console.log(num1+num2); /if your dosen't return any thing then result undefined 
+//     return
+// }
+// const result = add(13,12); 
+// console.log(result);
+
+
+// function add(num1 , num2){
+//     console.log(num1,num2);
+// }
+
+// const result = add(13); //if have missing any parameter then it will be undefine
+
+const premik = {name:"Rony", phone: 01684343}
+console.log(premik.gif);
+
+let fun =undefined
+console.log(fun)
+
+// If want to get null then it need to add or setup as you want.
